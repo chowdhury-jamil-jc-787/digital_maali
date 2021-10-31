74e6386716d79a6b8229984bd273c5412c9e9a45
unknown <17-34747-2@student.aiub.edu>
2021-10-31 19:26:36 +0600

    jc

--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,4 @@ Route::get('/messages','App\Http\Controllers\Messages@list')->name('messages.api
 Route::get('/send','App\Http\Controllers\Messages@insert')->name('send.api');
 
 
-Route::post('/signature','App\Http\Controllers\SignatureController@signature')->name('signature.api');
+Route::post('/request_otp','App\Http\Controllers\SignatureController@signature')->name('request_otp.api');
